72cdb22b6afddb62c6d20e70105431a9e7319318
apriljchang <chang.april07@gmail.com>
2026-05-22 16:51:33 -0400

    added title and date

--- src/pages/EntryDetails.jsx
+++ src/pages/EntryDetails.jsx
@@ -1,13 +1,31 @@
 import { useParams } from 'react-router-dom'
 import '../css/EntryDetails.css'
+import { useState, useEffect } from 'react'
 
 export default function EntryDetails() {
     const { id } = useParams()
+    const [movie, setMovie] = useState(null)
+
+    useEffect(() => {
+        const savedEntries = localStorage.getItem('entries')
+        if (savedEntries) {
+            const allEntries = (JSON.parse(savedEntries))
+
+            const foundMovie = allEntries.find(entry => entry.id === Number(id))
+
+            setMovie(foundMovie)
+        }  
+    }, [id])
+
+    if (!movie)
+    {
+        return <div className = "loading">Loading movie details...</div>
+    }
 
     return (
-        <div>
-            <h1>Entry Details Page</h1>
-            <p>Viewing ID: {id}</p>
+        <div className = " entry-details-container">
+            <h1 className = "movie-title">{movie.title}</h1>
+           <p className="movie-date">Watched on: {movie.date ? movie.date : "no date found"}</p>
         </div>
     )
 }
