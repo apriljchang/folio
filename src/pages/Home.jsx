15a8c2413f3d398a72a44409a2b5880d34bd5b42
apriljchang <chang.april07@gmail.com>
2026-08-20 16:12:44 -0400

    change home page to set up split screen for journal and shelves

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,22 @@
 import Hero from '../components/Hero'
+import shelves from '../components/shelves'
+import journal from '../components/journal'
 
 
 export default function Home() {
   return (
     <main>
-      <Hero />
+      <div className = "home">
+          <Hero />
+          <div className = "home-shelves">
+            <h1>shelves</h1>
+            <shelves />
+          </div>
+          <div className = "home-journal">
+            <h1>journal</h1>
+            <journal />
+          </div>
+      </div>
     </main>
   )
 }
